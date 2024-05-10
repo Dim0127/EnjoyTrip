ba752886a8163f5f8173bd0b15e0c1be77689075
Dim0127 <sjmrabbit@naver.com>
2024-05-10 15:43:27 +0900

    jimin/remove/remove older/내가 다 지움

--- js/key.js
+++
@@ -1,3 +0,0 @@
-const serviceKey =
-  "yDEHpK2ue3EeMWd%2BfriS%2FE%2FkJuzFbE0Pv%2B3asepwFNXzEDpmv3QoZzFsar2M0QJ%2BYvmf9xCrG7yvRRqd8UXFuw%3D%3D";
-// const kakaoKey = "----- 발급받은 KAKAO API KEY를 입력하세요 -----";
