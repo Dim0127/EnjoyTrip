675f904832bedb7510b8856a8ff03f8c5acf78ba
Dim0127 <sjmrabbit@naver.com>
2024-05-12 01:08:21 +0900

    jimin/frontend/kakaomap/create search and list

--- enjoytrip/src/App.vue
+++ enjoytrip/src/App.vue
@@ -2,13 +2,15 @@
 import { RouterLink, RouterView } from "vue-router";
 import Header from "@/components/layout/TheHeader.vue";
 import Footer from "@/components/layout/TheFooter.vue";
-import Kakao from "@/components/kakaos/KakaoMap.vue"
+import Kakao from "@/components/kakaos/KakaoMap3.vue"
 </script>
 
 <template>
   <Header></Header>
   <RouterView />
-  <Kakao></Kakao>
+  <div>
+    <Kakao></Kakao>
+  </div>
   <Footer></Footer>
 </template>
 
--- enjoytrip/src/components/kakaos/KakaoMap.vue
+++ enjoytrip/src/components/kakaos/KakaoMap.vue
@@ -1,17 +1,3 @@
-<template>
-  <div>
-    <div id="map"></div>
-    <div class="button-group">
-      <button @click="changeSize(0)">Hide</button>
-      <button @click="changeSize(400)">show</button>
-      <button @click="displayMarker(markerPositions1)">marker set 1</button>
-      <button @click="displayMarker(markerPositions2)">marker set 2</button>
-      <button @click="displayMarker([])">marker set 3 (empty)</button>
-      <button @click="displayInfoWindow">infowindow</button>
-    </div>
-  </div>
-</template>
-
 <script>
 import { toRaw } from "vue";
 export default {
@@ -41,32 +27,30 @@ export default {
       this.initMap();
     } else {
       const script = document.createElement("script");
-      /* global kakao */
       script.onload = () => kakao.maps.load(this.initMap);
       script.src =
       '//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=3b2905e963dece1f2a34a9b0c887904b&libraries=clusterer,drawing,services';
-;
       document.head.appendChild(script);
     }
   },
   methods: {
+    //지도 초기화
     initMap() {
       const container = document.getElementById("map");
       const options = {
         center: new kakao.maps.LatLng(33.450701, 126.570667),
         level: 5,
       };
-
-      //지도 객체를 등록합니다.
-      //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
       this.map = new kakao.maps.Map(container, options);
     },
+    //지도 크기 바꾸기
     changeSize(size) {
       const container = document.getElementById("map");
       container.style.width = `${size}px`;
       container.style.height = `${size}px`;
       toRaw(this.map).relayout();
     },
+    //마커 표시하기
     displayMarker(markerPositions) {
       if (this.markers.length > 0) {
         this.markers.forEach((marker) => marker.setMap(null));
@@ -93,6 +77,7 @@ export default {
         toRaw(this.map).setBounds(bounds);
       }
     },
+    //정보 표시하기
     displayInfoWindow() {
       if (this.infowindow && this.infowindow.getMap()) {
         //이미 생성한 인포윈도우가 있기 때문에 지도 중심좌표를 인포윈도우 좌표로 이동시킨다.
@@ -117,7 +102,20 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<template>
+  <div>
+    <div id="map"></div>
+    <div class="button-group">
+      <button @click="changeSize(0)">Hide</button>
+      <button @click="changeSize(400)">show</button>
+      <button @click="displayMarker(markerPositions1)">marker set 1</button>
+      <button @click="displayMarker(markerPositions2)">marker set 2</button>
+      <button @click="displayMarker([])">marker set 3 (empty)</button>
+      <button @click="displayInfoWindow">infowindow</button>
+    </div>
+  </div>
+</template>
+
 <style scoped>
 #map {
   width: 400px;
