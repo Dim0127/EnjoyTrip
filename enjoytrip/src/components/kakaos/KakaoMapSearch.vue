400b369f02dda4379b03d214a1faa22aca9f599a
Dim0127 <sjmrabbit@naver.com>
2024-05-15 01:24:48 +0900

    jimin/frontend/kakaomap/create marker, move center, change bound

--- enjoytrip/src/components/kakaos/KakaoMapSearch.vue
+++ enjoytrip/src/components/kakaos/KakaoMapSearch.vue
@@ -1,15 +1,18 @@
 <script setup>
-import { ref, onMounted, watch } from "vue";
+import { ref, onMounted, watchEffect } from "vue";
 import { isExist, createHotplace } from "@/api/hotplace.js";
 
-const map = ref(null);
-const ps = ref(null);
+const map = ref();
+const ps = ref();
+const bounds = ref();
 
 const places = ref([]);
 const keyword = ref("");
-const selectedPlace = ref(null);
+const selectedPlace = ref();
 const createHotplaceAvailable = ref(false);
 const goCreateReviewAvailable = ref(false);
+
+const markers = ref([]);
 
 const addScript = () => {
   const script = document.createElement("script");
@@ -28,6 +31,7 @@ const initMap = () => {
 
   map.value = new kakao.maps.Map(container, options);
   ps.value = new kakao.maps.services.Places();
+  bounds.value = new kakao.maps.LatLngBounds();
 
   createHotplaceAvailable.value = true;
   goCreateReviewAvailable.value = true;
@@ -37,12 +41,44 @@ onMounted(() => {
   !window.kakao || !window.kakao.maps ? addScript() : initMap();
 });
 
+const drawMarkers = () => {
+  if (markers.value.length > 0) {
+    for (const marker of markers.value) {
+      marker.setMap(map.value);
+    }
+  }
+};
+
+const removeMarkers = () => {
+  if (markers.value.length > 0) {
+    for (const marker of markers.value) {
+      marker.setMap(null);
+    }
+  }
+};
+
+const setMarkers = () => {
+  removeMarkers();
+  bounds.value = new kakao.maps.LatLngBounds(); // Reset bounds
+
+  for (const place of places.value) {
+    const markerPosition = new kakao.maps.LatLng(place.y, place.x);
+    const marker = new kakao.maps.Marker({ title: place.place_name, position: markerPosition });
+    markers.value.push(marker);
+    bounds.value.extend(markerPosition); // Extend bounds
+  }
+  drawMarkers();
+  map.value.setBounds(bounds.value);
+  map.value.setCenter(markers.value[0].getPosition());
+};
+
 const searchPlacesByKeyword = (newPlaces, status) => {
   if (status === kakao.maps.services.Status.OK) {
     for (const newPlace of newPlaces) {
       newPlace["isSelected"] = false;
     }
     places.value = newPlaces;
+    setMarkers();
   } else if (status === kakao.maps.services.Status.ERROR) {
     alert("검색 결과 중 오류가 발생했습니다.");
     return;
@@ -52,8 +88,11 @@ const searchPlacesByKeyword = (newPlaces, status) => {
 const searchPlaces = () => {
   selectedPlace.value = null;
   places.value = [];
+  removeMarkers();
+  markers.value = [];
+  bounds.value = new kakao.maps.LatLngBounds();
 
-  if (!keyword.value.replace(/^\s+|\s+$/g, "")) {
+  if (!keyword.value.trim()) {
     alert("키워드를 입력해주세요!");
     return false;
   }
@@ -99,17 +138,11 @@ const plzCreateHotplace = async () => {
   }
 };
 
-watch(selectedPlace, async (newSelectedPlace) => {
-  if (newSelectedPlace !== null) {
-    const isExistHotplace = await checkHotplace(newSelectedPlace.id);
-    console.log(isExistHotplace);
-    if (isExistHotplace) {
-      createHotplaceAvailable.value = true;
-      goCreateReviewAvailable.value = false;
-    } else {
-      createHotplaceAvailable.value = false;
-      goCreateReviewAvailable.value = true;
-    }
+watchEffect(async () => {
+  if (selectedPlace.value !== null) {
+    const isExistHotplace = await checkHotplace(selectedPlace.value.id);
+    createHotplaceAvailable.value = isExistHotplace;
+    goCreateReviewAvailable.value = !isExistHotplace;
   } else {
     createHotplaceAvailable.value = true;
     goCreateReviewAvailable.value = true;
