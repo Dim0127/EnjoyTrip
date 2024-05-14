46a352e749d88d33e6469dcc427373ec0471fbfd
Dim0127 <sjmrabbit@naver.com>
2024-05-14 10:42:50 +0900

    jimin/frontend/css/put background, change icon

--- enjoytrip/src/App.vue
+++ enjoytrip/src/App.vue
@@ -1,20 +1,21 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import Header from "@/components/layout/TheHeader.vue";
-import FooterCentered from "@/components/layout/TheCenteredFooter.vue";
+
+import Header from "@/components/layouts/TheHeader.vue";
+import FooterCentered from "@/components/layouts/TheCenteredFooter.vue";
 </script>
 
 <template>
   <div class="container position-sticky z-index-sticky top-0">
     <div class="row">
       <div class="col-12">
-        <Header :sticky="true"/>
+        <Header :sticky="true" />
       </div>
     </div>
   </div>
 
   <RouterView />
-  
+
   <FooterCentered></FooterCentered>
 </template>
 
--- enjoytrip/src/components/hotplaces/HotplaceSearch.vue
+++ enjoytrip/src/components/hotplaces/HotplaceSearch.vue
@@ -1,12 +1,30 @@
 <script setup>
-import KakaoMap from "@/components/kakaos/KakaoMap.vue"
+import KakaoMap from "@/components/kakaos/KakaoMap.vue";
 </script>
 
 <template>
-  <div>
-    핫플서치입니다
+  <div
+    class="page-header min-vh-75"
+    style="
+      background-image: url('https://flexible.img.hani.co.kr/flexible/normal/700/466/imgdb/original/2021/0603/20210603504045.jpg');
+    "
+    loading="lazy"
+  >
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 text-center mx-auto position-relative">
+          <h3 class="text-white pt-3 mt-n5 me-2" :style="{ display: 'inline-block ' }">
+            이곳은 카카오 맵 뷰를 담은 핫플레이스 뷰입니다.
+          </h3>
+        </div>
+      </div>
+    </div>
   </div>
-  <KakaoMap></KakaoMap>
+  <div class="container mt-5">
+    <div class="row">
+      <KakaoMap></KakaoMap>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
