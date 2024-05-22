10dd40ca5b94761e4ce9a612082fa08c82557340
vnfmslss <vnfmslss@naver.com>
2024-05-22 18:07:10 +0900

    eiseul/frontend/사진추가

--- enjoytrip/src/views/TheMainView.vue
+++ enjoytrip/src/views/TheMainView.vue
@@ -1,11 +1,10 @@
 <script setup>
 import OpenAI from "@/components/openais/OpenAI.vue"
+import image from "@/assets/img/bg_hotplace.jpg";
 </script>
 
 <template>
-  <div class="page-header min-vh-100" style="
-      background-image: url('https://flexible.img.hani.co.kr/flexible/normal/700/466/imgdb/original/2021/0603/20210603504045.jpg');
-    " loading="lazy">
+  <div class="page-header min-vh-100" :style="{ 'background-image': 'url(' + image + ')' }" loading="lazy">
     <div class="container">
       <div class="row">
         <div class="col-lg-7 text-center mx-auto position-relative">
